cdb2f8fb1c918fba0cd9e3d318b3a50a7a0c72cd
20211tiimi0184 <hariandami@gmail.com>
2023-04-28 08:21:34 -0300

    tem q modificar o menu

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,38 @@
 const btnMenu = document.getElementById('btnMenu');
+const materiasLista = document.getElementById('materiasLista')
 
 function mostraLista(){
-    const divOpcoes = document.getElementById('opcoes');
-    if(divOpcoes.style.display == 'block'){
-        divOpcoes.style.display = 'none';
-    } else{
-        divOpcoes.style.display = 'block';
+    var toggler = document.getElementsByClassName("caret");
+    var i;
+    
+    for (i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+      });
     }
+}
+
+function mostraListaMaterias(){
+    // let divOpcoesLista = document.getElementById('opcoesLista');
+    // let divOpcoes = document.getElementById('opcoes');
+    // divOpcoes.style.display = 'block';     
+    // divOpcoesLista.style.visibility = "block";
+
+    // alert(divOpcoesLista.style.display);
+
+
+    // if(divOpcoesLista.style.display == 'block'){
+    //     divOpcoesLista.style.display = 'none';
+    //     alert("entrei");
+    // } else{
+    //     divOpcoes.style.display = 'block';     
+        
+    //     divOpcoesLista.style.display = 'block';
+    //     alert(  divOpcoes.style.display );
+    //     alert("entrei1");
+    // }
+     
+    // alert(divOpcoesLista.style.display);
+
 }
