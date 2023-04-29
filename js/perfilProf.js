bf434625194bd9af90181876b2b79fda71fc4202
Harian <harianadami@gmail.com>
2023-04-28 23:10:28 -0300

    Trabalho Finalizado

--- js/perfilProf.js
+++ js/perfilProf.js
@@ -1,3 +1,17 @@
+/* funcao que permite a visualzacao da div com as opcoes de perfil e cadastre-se */
+
+function mostraLogin(){
+    document.getElementById("perfil_principal").classList.toggle("show");
+}
+
+/* funcao para alertar o usuario a se cadastrar*/
+
+function alertCadastro(){
+    alert("Você precisa se cadastrar primeiro!");
+}
+
+/* funcao usada para botoes que ainda nao possuem funcionalidade*/
+
 function alerta(){
     alert("Função Indisponível no momento");
 }
