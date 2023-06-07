a11ee3ae8d878fde4233a0f120008340d537de11
harianadm <91471333+harianadm@users.noreply.github.com>
2023-06-07 10:43:53 -0300

    Update TesteExceptionLogin.php

--- tests/acceptance/TesteExceptionLogin.php
+++ tests/acceptance/TesteExceptionLogin.php
@@ -1,1 +1,24 @@
+<?php
 
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class BuscaCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function buscarResultadosNaPaginaTest(AcceptanceTester $I)
+    {
+		
+          $I = new AcceptanceTester($scenario);
+          $I->amOnPage('/');
+          $I->click('Login');
+          $I->seeCurrentURLEquals('/menu.html');
+
+    }
+}
