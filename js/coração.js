c7f68643ba1f9db06375e44a90f77d2bd6b0d299
Harian <harianadami@gmail.com>
2023-07-03 07:54:38 -0300

    upado

--- js/coração.js
+++ js/coração.js
@@ -1,5 +1,8 @@
 document.querySelectorAll('.like-button').forEach(button => {
+// Seleciona todos os elementos com a classe 'like-button' e itera sobre cada um deles usando o método 'forEach'
   button.addEventListener('click', () => {
+  // Adiciona um ouvinte de evento de clique a cada botão
     button.classList.toggle('red');
+    // Alterna a classe 'red' no botão atual, adicionando-a se não estiver presente e removendo-a se já estiver presente
   });
 });
