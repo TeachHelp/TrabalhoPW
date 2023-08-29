2c75031683b88935b9460fa5f801d9a6e6ff1d08
= <sallescardosodavi@gmail.com>
2023-08-29 11:12:45 -0300

    footer

--- menuBootstrap.php
+++ menuBootstrap.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />"
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="css/menuBootstrap.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer></script>
@@ -123,7 +124,109 @@
                   <h3 class="">Informática</h3>
                 </div>
               </div>
+
+
+              
+              <div class="row">.</div>
+              <!--segunda linha, com os elementos igualmente distribuídos das pontas ao meio-->
+              <div class="row justify-content-between">
+              <!--div de história, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
+              <div class="card" id="card-custom-historia" style="width: 15rem;" onclick="alerta()" type="button">
+                <img src="img/hist.jpg" class="img card-img-top" alt="Historia foto">
+                <div class="card-title titulo fs-5">
+                  <h3 class="">História</h3>
+                </div>
+              <!--div de inglês, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
+              </div>
+              <div class="card" id="card-custom-ingles" style="width: 15rem;" onclick="alerta()" type="button">
+                <img src="img/ing.jpg" class="img card-img-top" alt="Ingles foto">
+                <div class="card-title titulo fs-5">
+                  <h3 class="">Inglês</h3>
+                </div>
+              <!--div de idiomas, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
+              </div>
+              <div class="card" id="card-custom-idiomas" style="width: 15rem;" onclick="alerta()" type="button">
+                <img src="img/idiom.jpg" class="img card-img-top" alt="Idiomas foto">
+                <div class="card-title titulo fs-5">
+                  <h3 class="">Idiomas</h3>
+                </div>
+              <!--div de informática, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
+              </div>
+              <div class="card" id="card-custom-informatica" style="width: 15rem;" onclick="alerta()" type="button">
+                <img src="img/info.jpeg" class="img card-img-top" alt="Informatica foto">
+                <div class="card-title titulo fs-5">
+                  <h3 class="">Informática</h3>
+                </div>
+              </div>
           </div>
+          <main></main>
+          <footer>
+            <div id="footer_content">
+              <div id="footer_contacts">
+                <h1>Logo</h1>
+                <p>apenas um teste</p>
+                <div id="footer_social_media">
+                  <a href="#" class="footer_link" id="instagram">
+                    <i class="fa-brands fa-instagram"></i>
+                  </a>
+                  <a href="#" class="footer_link" id="facebook">
+                    <i class="fa-brands fa-facebook-f"></i>
+                  </a>
+                  <a href="#" class="footer_link" id="whatsapp">
+                    <i class="fa-brands fa-whatsapp"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <ul class="footer-list">
+              <li>
+                <h3>Blog</h3
+              </li>
+              <li>
+                <a href="#" class="footer-link">Item1</a>
+              </li>
+              <li>
+                <a href="#" class="footer-link">Item2</a>
+              </li>
+              <li>
+                <a href="#" class="footer-link">Item3</a>
+              </li>
+
+            </ul>
+            <ul class="footer-list">
+              <li>
+                <h3>Products</h3
+              </li>
+              <li>
+                <a href="#" class="footer-link">Item4</a>
+              </li>
+              <li>
+                <a href="#" class="footer-link">Item5</a>
+              </li>
+              <li>
+                <a href="#" class="footer-link">Item6</a>
+              </li>
+
+            </ul>
+            <div id="footer_subscribe">
+              <h3>Teste</h3>
+              <p>
+                Olá teste teste teste teste teste 
+              </p>
+              <div id="input_group">
+                <input type="email" id="email">
+                <button>
+                  <i class="fa-regular fa-envelope"></i>
+                </button>
+
+              </div>
+
+            </div>
+            <div id="footer_copyright">
+              &#169
+              2023 all rights reserved
+            </div>
+          </footer>
 
 </body>
 
