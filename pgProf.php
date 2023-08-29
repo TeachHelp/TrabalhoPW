45330f2ef18027c0863e965a788a49d859b0587e
Evelyn Pereira <103004390+Evelyn3po@users.noreply.github.com>
2023-08-29 10:37:56 -0300

    Update pgProf.php

--- pgProf.php
+++ pgProf.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Área de Matemática</title>
+	<title>Professores de Matemática</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/pgProf.css">
 	<link rel="icon" type="image/x-icon" href="img/logo.png">
