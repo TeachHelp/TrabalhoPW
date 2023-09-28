24295e3d7558eb4231cd70b0cee357720f44c433
20211tiimi0184 <hariandami@gmail.com>
2023-09-28 12:15:17 -0300

    pronto só faltam ajustes

--- sobre.php
+++ sobre.php
@@ -8,12 +8,20 @@
         <!--Div com a logo e com um texto de apresentação-->
         <div class="divApresentacao col-sm-12 col-12 bg-txt p-1">
             <p class="tit"> Sobre o TeachHelp  </p>
-            <p class="pTxt"> Bem-vindo ao TeachHelp, o seu destino central para encontrar instrutores particulares em uma ampla variedade de campos, 
-                tanto dentro como fora do âmbito acadêmico. Com o nosso slogan "Auxiliando estudantes e Empregando capacitados", 
-                nosso objetivo é facilitar a busca por instrutores altamente qualificados e experientes, 
-                prontos para ajudar você a alcançar seus objetivos de aprendizado. Quer você esteja buscando apoio acadêmico, 
-                desenvolvimento de habilidades, ou simplesmente deseje aprender algo novo, o TeachHelp é o lugar onde a excelência educacional encontra a conveniência. 
-                Cadastre-se hoje mesmo e comece sua jornada de aprendizado conosco!
+            <p class="pTxt"> Bem-vindo ao TeachHelp, a sua plataforma de referência para conectar estudantes e profissionais capacitados de diversos campos. 
+                Nosso lema, "Auxiliando estudantes e Empregando capacitados," reflete nossa missão de fornecer um espaço onde qualquer pessoa pode buscar 
+                instrutores particulares altamente qualificados. Quer você esteja em busca de suporte acadêmico, 
+                deseje aprimorar suas habilidades em uma área específica, ou queira aprender algo completamente novo, o TeachHelp é o seu destino confiável.</p>
+
+            <p class="pTxt"> No TeachHelp, tornamos o processo de encontrar instrutores particulares simples e conveniente. 
+                Com um registro gratuito, você pode explorar nossa vasta rede de instrutores em áreas que vão desde matemática e ciências até música e idiomas. 
+                Nossa plataforma permite que você refine sua pesquisa com base em suas necessidades específicas, como localização, disponibilidade, e preço, 
+                tornando mais fácil do que nunca encontrar o instrutor perfeito para suas metas de aprendizado. </p> 
+
+            <p class="pTxt"> O TeachHelp não se limita apenas ao âmbito acadêmico; também oferecemos instrutores em áreas práticas, como artesanato, culinária, programação e muito mais. 
+                Acreditamos que o aprendizado é uma jornada contínua, e nosso compromisso é fornecer as ferramentas e recursos necessários 
+                para que você alcance o sucesso em seus objetivos educacionais e de desenvolvimento pessoal. 
+                Junte-se a nós hoje e descubra a diferença que o TeachHelp pode fazer em sua jornada de aprendizado. </p>
             </p>
         </div>
     </div>
