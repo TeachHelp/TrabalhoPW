ba90e69e2a613735cee9ce8f1be1fe8e0eaec195
20211tiimi0184 <hariandami@gmail.com>
2023-11-10 12:49:52 -0300

    conexao 2

--- conexaobd.php
+++ conexaobd.php
@@ -1,9 +1,9 @@
 <?php
 //Conexão com banco de dados
-$servername = "sql303.infinityfree.com"; //endereço do servidor
-$username="if0_35400743";
-$password="46fZd3izCXd";
-$db_name="if0_35400743_Teachhelp";
+$servername = "db4free.net"; //endereço do servidor
+$username="teachhelp";
+$password="d4pnw-d3@*ZRNkF";
+$db_name="teachhelp";
 
 //pdo - somente orientado objeto
 $connect =mysqli_connect($servername,$username,$password,$db_name);
