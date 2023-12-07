1c89fdcd6a7a409ab51071d57a9733ab17ead193
20201tiimi0047 <sallescardosodavi@gmail.com>
2023-12-07 12:55:18 -0300

    funcionando tela dos professores

--- menuBootstrap.php
+++ menuBootstrap.php
@@ -11,100 +11,125 @@
 <div class="container justify-content">
   <!--primeira linha, com os elementos espalhados igualmente dos cantos ao centro do container-->
   <div class="row justify-content-between">
-  
-      <div class="card" id="card-custom-portugues" style="width: 15rem;" onclick="alerta()" type="button">
+      
+      <div class="card" id="card-custom-portugues" style="width: 15rem;" type="button">
+        <a class ="materia" href="pgProf2.php?materia='Português'">
         <img src="img/port.jpg" class=" img card-img-top" alt="portugues foto">
         <div class="card-title titulo fs-5">
           <h3 class="">Português</h3>
         </div>
+        </a>
       </div>
+
       <!--div de matemática, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-matematica" style="width: 15rem;" onclick="telaProfs()" type="button">
-        <img src="img/mat.jpg" class="img card-img-top" alt="Matematica foto">
-        <div class="card-title titulo fs-5">
-          <h3 class="">Matemática</h3>
+        <div class="card" id="card-custom-matematica" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Matemática'">
+          <img src="img/mat.jpg" class="img card-img-top" alt="Matematica foto">
+          <div class="card-title titulo fs-5">
+            <h3 class="">Matemática</h3>
+          </div>
+          </a>
         </div>
-      </div>
       <!--div de música, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-musica" style="width: 15rem;" onclick="alerta()" type="button">
-        <img src="img/music.jpg" class=" img card-img-top " alt="musica foto">
-        <div class="card-title titulo fs-5">
-          <h3 class="">Música</h3>
+        <div class="card" id="card-custom-musica" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Música'">
+          <img src="img/music.jpg" class=" img card-img-top " alt="musica foto">
+          <div class="card-title titulo fs-5">
+            <h3 class="">Música</h3>
+          </div>
+          </a>
         </div>
-      </div>
       <!--div de esportes, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-esportes" style="width: 15rem;" onclick="alerta()" type="button">
-        <img src="img/esport.jpg" class="img card-img-top" alt="Esportes foto">
-        <div class="card-title titulo fs-5">
-          <h3 class="">Esportes</h3>
+        <div class="card" id="card-custom-esportes" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Esportes'">
+          <img src="img/esport.jpg" class="img card-img-top" alt="Esportes foto">
+          <div class="card-title titulo fs-5">
+            <h3 class="">Esportes</h3>
+          </div>
+          </a>
         </div>
-      </div>
   </div>
   <!--linha de quebra para gerar espaçamento-->
-  <div class="row">.</div>
+  <div class="row"><br></div>
   <!--segunda linha, com os elementos igualmente distribuídos das pontas ao meio-->
   <div class="row justify-content-between">
       <!--div de história, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-historia" style="width: 15rem;" onclick="alerta()" type="button">
+      <div class="card" id="card-custom-historia" style="width: 15rem;" type="button">
+      <a class ="materia" href="pgProf2.php?materia='História'">
         <img src="img/hist.jpg" class="img card-img-top" alt="Historia foto">
         <div class="card-title titulo fs-5">
           <h3 class="">História</h3>
-        </div>
+        </div> 
+        </a>
       </div>
       <!--div de inglês, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-ingles" style="width: 15rem;" onclick="alerta()" type="button">
+      <div class="card" id="card-custom-ingles" style="width: 15rem;" type="button">
+        <a class ="materia" href="pgProf2.php?materia='Inglês'">
         <img src="img/ing.jpg" class="img card-img-top" alt="Ingles foto">
         <div class="card-title titulo fs-5">
           <h3 class="">Inglês</h3>
         </div>
+        </a>
       </div>
       <!--div de idiomas, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-idiomas" style="width: 15rem;" onclick="alerta()" type="button">
+      <div class="card" id="card-custom-idiomas" style="width: 15rem;" type="button">
+        <a class ="materia" href="pgProf2.php?materia='Idiomas'">
         <img src="img/idiom.jpg" class="img card-img-top" alt="Idiomas foto">
         <div class="card-title titulo fs-5">
           <h3 class="">Idiomas</h3>
         </div>
+        </a>
       </div>
       <!--div de informática, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-      <div class="card" id="card-custom-informatica" style="width: 15rem;" onclick="alerta()" type="button">
+      <div class="card" id="card-custom-informatica" style="width: 15rem;" type="button">
+        <a class ="materia" href="pgProf2.php?materia='Informática'">
         <img src="img/info.jpeg" class="img card-img-top" alt="Informatica foto">
         <div class="card-title titulo fs-5">
           <h3 class="">Informática</h3>
         </div>
+        </a>
       </div>
   </div>
 
       
-      <div class="row-1">.</div>
+      <div class="row-1"><br></div>
       <!--segunda linha, com os elementos igualmente distribuídos das pontas ao meio-->
       <div class="row justify-content-between">
         <!--div de geografia, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-        <div class="card" id="card-custom-geografia" style="width: 15rem;" onclick="alerta()" type="button">
+        <div class="card" id="card-custom-geografia" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Geografia'">
           <img src="img/geo.jpg" class="img card-img-top" alt="Geografia foto">
           <div class="card-title titulo fs-5">
             <h3 class="">Geografia</h3>
           </div>
+          </a>
         </div>
         <!--div de química, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-        <div class="card" id="card-custom-quimica" style="width: 15rem;" onclick="alerta()" type="button">
+        <div class="card" id="card-custom-quimica" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Química'">
           <img src="img/Quim.jpg" class="img card-img-top" alt="Quimica foto">
           <div class="card-title titulo fs-5">
             <h3 class="">Química</h3>
           </div>
+          </a>
         </div>
         <!--div de biologia, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-        <div class="card" id="card-custom-biologia" style="width: 15rem;" onclick="alerta()" type="button">
+        <div class="card" id="card-custom-biologia" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Biologia'">
           <img src="img/bio.jpg" class="img card-img-top" alt="Biologia foto">
           <div class="card-title titulo fs-5">
             <h3 class="">Biologia</h3>
           </div>
+          </a>
         </div>
         <!--div de física, que contém um card estilizado, uma imagem e uma escrita em h3 do nome da matéria-->
-        <div class="card" id="card-custom-fisica" style="width: 15rem;" onclick="alerta()" type="button">
+        <div class="card" id="card-custom-fisica" style="width: 15rem;" type="button">
+          <a class ="materia" href="pgProf2.php?materia='Física'">
           <img src="img/fisica.jpg" class="img card-img-top" alt="Fisica foto">
           <div class="card-title titulo fs-5">
             <h3 class="">Física</h3>
           </div>
+          </a>
         </div>
       </div>
   </div>
--- pgProf2.php
+++ pgProf2.php
@@ -1,7 +1,12 @@
 <?php include_once 'header.php';
-$consultaProfs = "SELECT * FROM alunos WHERE professores = 'sim';";
+//$consultaProfs = "SELECT * FROM alunos WHERE professor = 'sim';";
 
+$materia = $_GET['materia'];
+//$consultaProfs = "SELECT instrutores.* FROM instrutores INNER JOIN alunos ON instrutores.fk_id_aluno = alunos.id WHERE instrutores.materia = $materia AND alunos.professor = 'sim';";
+$consultaProfs = "SELECT alunos.*, instrutores.* FROM alunos INNER JOIN instrutores ON alunos.id = instrutores.fk_id_aluno WHERE instrutores.materia = $materia AND alunos.professor = 'sim' ";
 
+echo $consultaProfs;
+$resultado = mysqli_query($connect,$consultaProfs);
 ?>
 <link href="css/pgProf.css" rel="stylesheet">
 <script type="text/javascript" src="js/pgProf.js" defer></script>
@@ -9,56 +14,24 @@ $consultaProfs = "SELECT * FROM alunos WHERE professores = 'sim';";
 <!-- Div criada para armazenar as divs de cada professor -->
 	<div class="card-container">
 		<!-- Div card com as informações do professor individual -->
-		<div class="card">
-			<a href="perfilProf.php">
-			<!-- Imagem do professor -->
-			<img src="img/ImgFidelis.jpg" alt="Fidelis"  class="card-image">
-			<!-- Div que fica com o nome e fomação do professor -->
-			<div class="card-desc">
-				<h4>Fidelis Zanetti<i class="fa fa-heart like-button"></i></h4>
-				<p>Professor doutor em Matemática Aplicada formado pela Unicamp</p>
-			</div>
-			</a>
-		<!-- Fechando card --> 
-		</div>
-		<div class="card">
-		    <img class="card-image" src="https://pbs.twimg.com/profile_images/1379776286709723136/J8zPfaO1_400x400.jpg">
-			<div class="card-desc">
-				<h4>Rafael Procópio<i class="fa fa-heart like-button"></i></h4>
-				<p>Professor de Matemática formado pela UGF e com especialização no ensino da Matemática pela UFRJ</p>
-			</div>
-		</div>
-		<div class="card">
-		    <img class="card-image" src="https://img.r7.com/images/sandro-curio-tiktok-01072021221814525?dimensions=299x417">
-			<div class="card-desc">
-				<h4>Sandro Curió<i class="fa fa-heart like-button"></i></h4>
-				<p>Professor de Matemática formado pela UFF</p>
-			</div>
-		</div>
-	</div>
-	<div class="card-container">
-		<div class="card">
-			<img class="card-image" src="https://media.licdn.com/dms/image/D4D03AQGJCFWCpMoDaA/profile-displayphoto-shrink_800_800/0/1691114540727?e=2147483647&v=beta&t=FNRkVuHbVMBtVoQpBOfhSkNYd4tUTFiC5I54lOQ1WL0">
-			<div class="card-desc">
-				<h4>Gabriele Gonçalves<i class="fa fa-heart like-button"></i></h4>
-				<p>Professora formada em Matématica pelo IFES</p>
-			</div>
-		</div>
-		<div class="card">
-			<img class="card-image" src="https://yt3.ggpht.com/TRwG_DfRgoaY2zgt53ws_LJ3s2WvI0jlsOOG4vhKGpNe5Vwi8Gf9Hi45TQOjhIHooGFk2cB0BLCv6YE=s640-c-fcrop64=1,00000000ffffffff-nd-v1">
-			<div class="card-desc">
-				<h4>Paulo Pereira<i class="fa fa-heart like-button"></i></h4>
-				<p>Mestre em Matemática pelo Instituto Nacional de Matemática Pura e Aplicada - IMPA</p>
-			</div>
-		</div>
-		<div class="card">
-		    <img class="card-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTVQ7km6Rf05R54qlITOecXx49xRRlsYNNAA&usqp=CAU">
-			<div class="card-desc">
-				<h4>Daniel Ferretto<i class="fa fa-heart like-button"></i></h4>
-				<p>Professor formado em Matemática pela UFSC</p>
-			</div>
-		</div>
-	</div>
+		<?php 			
+			if (mysqli_num_rows($resultado)>0){                                               
+				while($linha =mysqli_fetch_array($resultado)){
+					echo '<div class="card cartaoProf">';
+						echo '<a href="perfilProf.php?id=' . $linha['id'] .'">';
+						//imagem do professor
+						echo '<img src="' . $linha['foto'] . '" alt="Fidelis"  class="card-image">';
+						//Div que fica com o nome e fomação do professor
+						echo '<div class="card-desc">';
+							echo '<h4>' . $linha['nome'] . '<i class="fa fa-heart like-button"></i></h4>';
+							echo '<p>' . $linha['descricao'] . '</p>';
+						echo '</div>';
+						echo '</a>';
+					echo '</div>';
+				}
+			}
+		?>
+	</div> 
 <!-- Puxando pagina js -->
 <script src="js/coração.js"></script>
 
